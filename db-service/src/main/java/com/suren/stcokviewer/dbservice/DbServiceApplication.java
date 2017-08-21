7f39bb1cca0039048137cbd6f6484c0031ca6d9d
surendirababu <babusuren@hotmail.com>
2017-08-21 09:14:52 +0530

    Added DB service as eureka client for the eureka server

--- db-service/src/main/java/com/suren/stcokviewer/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/suren/stcokviewer/dbservice/DbServiceApplication.java
@@ -2,9 +2,11 @@ package com.suren.stcokviewer.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.suren.stcokviewer.dbservice.repository")
 public class DbServiceApplication {
 
--- db-service/src/main/java/com/suren/stcokviewer/dbservice/Quote.java
+++
@@ -1,7 +0,0 @@
-package com.suren.stcokviewer.dbservice;
-
-/**
- * Created by Surendirababu Janarthanan on 8/13/2017.
- */
-public class Quote {
-}
